7d25dde4523f3fa8d0f81ce645a790369be03606
fco.de.santiago.d@gmail.com <fco.de.santiago.d@gmail.com>
2015-07-30 13:33:04 -0400

    Initial for error

---
+++ openacademy/model/openacademy_course.py
@@ -0,0 +1,9 @@
+from openerp import models, fields, api
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
